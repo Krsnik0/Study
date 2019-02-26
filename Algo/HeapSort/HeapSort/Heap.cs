3902a16c3e800721a24a33f76c1b09d1eaa2ea12
Krsnik0 <soohyok2006@naver.com>
2019-02-26 19:29:35 +0900

    implement of HeapSort

---
+++ Algo/HeapSort/HeapSort/Heap.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2 {
+	class Heap {
+	
+		public int Parent(int i) {return (i - 1) / 2;}
+		public int Left(int i) {return i * 2 + 1;}
+		public int Right(int i) {return i * 2 + 2;}
+		public void Swap(ref int a, ref int b) {int tmp = a; a = b; b = tmp;}
+
+		public void MaxHeapify(int[] arr, int index, int heapSize) {
+			if (Left(index) >= heapSize) return;
+			int largest = index;
+			if (Left(index) < heapSize) {
+				if (arr[index] < arr[Left(index)]) {
+					largest = Left(index);
+					if (Right(index) < heapSize) {
+						if (arr[largest] < arr[Right(index)]) largest = Right(index);
+					}					
+				}
+				else {
+					if (Right(index) < heapSize) {
+						if (arr[largest] < arr[Right(index)]) largest = Right(index);
+					}
+				}
+			}
+			
+
+			if (largest == index) return;
+			else if (largest == Left(index)) {
+				Swap(ref arr[index], ref arr[largest]);
+				MaxHeapify(arr, Left(index), heapSize);
+			}
+			else if (largest == Right(index)) {
+				Swap(ref arr[index], ref arr[largest]);
+				MaxHeapify(arr, Right(index), heapSize);
+			}
+		}
+
+		public void HeapSort(int[] arr, int heapSize) {
+			for (int i = Parent(heapSize - 1); i >= 0; i--) {
+				MaxHeapify(arr, i, heapSize);
+			} // Build MaxHeap
+			for (int j = arr.Length - 1; j > 0; j--) {
+				Swap(ref arr[0], ref arr[j]);
+				heapSize--;
+				MaxHeapify(arr, 0, heapSize);
+			}
+		}
+	}
+}
---
+++ Algo/HeapSort/HeapSort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2 {
+	class Program {
+		static void Main(string[] args) {
+			var heap = new Heap();
+
+			int[] a = new int[] { 6, 3, 11 , 10, 99, 2, 1, 7, 4, 35, 83, 13, 37, 17, 11};
+			Console.Write("Before:");
+			foreach (var item in a)
+				Console.Write(" " + item);
+			Console.WriteLine();
+
+			heap.HeapSort(a, a.Length);
+
+			Console.Write("After:");
+			foreach (var item in a)
+				Console.Write(" " + item);
+			Console.WriteLine();
+
+		}
+	}
+}
