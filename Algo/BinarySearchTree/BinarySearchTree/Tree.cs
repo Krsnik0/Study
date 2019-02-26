efb17e20fbbd077e24c1613f1c7eee27defc20e3
Krsnik0 <soohyok2006@naver.com>
2019-02-26 19:49:08 +0900

    updating BST

---
+++ Algo/BinarySearchTree/BinarySearchTree/Tree.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree {
+	class Tree {
+		public void InorderWalk() {
+
+		}
+
+		public void Search() {
+
+		}
+
+		public void IterativeSearch() {
+
+		}
+
+		public void Min() {
+
+		}
+
+		public void Max() {
+
+		}
+
+		public void Successor() {
+
+		}
+
+		public void Predecessor() {
+
+		}
+	}
+}
