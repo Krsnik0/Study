ecf6d8903a56f2d5e863b824b80a261654029dcf
Krsnik0 <soohyok2006@naver.com>
2019-04-06 00:06:39 +0900

    Implement of RBT(incompleted)

---
+++ Algo/RedBlackTree/RedBlackTree/Node.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedBlackTree {
+	class Node {
+
+		public void LeftRotate() {
+
+		}
+
+		public void RightRotate() {
+
+		}
+
+		public void Insert() {
+
+		}
+
+		public void InsertFixup() {
+
+		}
+
+		public void Transplant() {
+
+		}
+
+		public void Delete() {
+
+		}
+
+		public void DeleteFixup() {
+
+		}
+	}
+}
