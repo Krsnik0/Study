8a41cd43388e40f22e332eceb7e506a43dc3ac38
Krsnik0 <soohyok2006@naver.com>
2019-11-08 22:33:52 +0900

    Daily Baekjoon Challenge 9465, 2156

---
+++ Baekjoon/2156.cpp
@@ -0,0 +1,58 @@
+// https://www.acmicpc.net/problem/2156
+// Written by JSH, Krsnik
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+class sommelier {
+private:
+	int n;
+	int* input;
+	int** ans;
+public:
+	sommelier(int n) {
+		this->n = n;
+		input = new int[n];
+		for (int i = 0; i < n; i++) {
+			scanf("%d", input + i);
+		}
+
+		ans = new int* [2];
+		for (int i = 0; i < 2; i++) {
+			ans[i] = new int[n];
+			memset(ans[i], 0, sizeof(int) * n);
+		}
+	}
+	void solve() {
+		ans[1][0] = input[0];
+		ans[0][1] = input[0];
+		ans[1][1] = input[0] + input[1];
+		for (int i = 2; i < n; i++) {
+			ans[0][i] = max(ans[0][i - 1], ans[1][i - 1]);
+			ans[1][i] = max(max(ans[0][i - 2], ans[1][i - 2]), ans[0][i - 2] + input[i - 1]) + input[i];
+		}
+
+		printf("%d\n", max(ans[0][n - 1], ans[1][n - 1]));
+	}
+	~sommelier() {
+
+		delete[] input;
+		for (int i = 0; i < 2; i++) {
+			delete[] ans[i];
+		}
+		delete[] ans;
+	}
+};
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	sommelier aaa(n);
+	aaa.solve();
+
+	return 0;
+}
