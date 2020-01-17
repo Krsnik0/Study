e03277f3ed8b019529c7bc12b7caa17b7032d98e
Krsnik0 <soohyok2006@naver.com>
2020-01-17 21:24:51 +0900

    update 1/17

--- Baekjoon/1451.cpp
+++ Baekjoon/1451.cpp
@@ -11,6 +11,7 @@
 #include <utility>
 #include <cmath>
 using namespace std;
+using ll = long long int;
 
 bool cmp(vector<int> v1, vector<int> v2) {
 	return v1[1] < v2[1];
@@ -22,7 +23,7 @@ int main() {
 
 	int n, m;
 	cin >> n >> m;
-	
+
 	vector<vector<int>> v(n);
 	for (auto& item : v)
 		item.resize(m);
@@ -31,27 +32,115 @@ int main() {
 			cin >> v[i][j];
 		}
 	}
-	int ans = 0;
+	ll ans = 0, tmp = 0;
+	int sum1 = 0, sum2 = 0, sum3 = 0;
 	//case: ㅗ ㅜ ㅓ ㅏ = || total 6
 
 	//case1 ㅜ
 	if (n > 1) {
-		int sum1 = 0, sum2 = 0, sum3 = 0;
 		for (int i = 0; i < n - 1; ++i) {
 			sum1 = 0;
 			for (int j = 0; j < i + 1; ++j) {
 				for (int k = 0; k < m; ++k) {
 					sum1 += v[j][k];
-				}				
+				}
 			}
+			for (int k = 0; k < m - 1; ++k) {
+				sum2 = 0, sum3 = 0;
+				for (int l = 0; l < k + 1; ++l) {
+					for (int j = i + 1; j < n; ++j) {
+						sum2 += v[j][l];
+					}
+				}
+				for (int l = k + 1; l < m; ++l) {
+					for (int j = i + 1; j < n; ++j) {
+						sum3 += v[j][l];
+					}
+				}
+				tmp = (ll)sum1 * (ll)sum2 * (ll)sum3;
+				ans = ans > tmp ? ans : tmp;
+			}
+		}
+	}
+	//case2 ㅗ
+	if (n > 1) {
+		for (int i = 0; i < n - 1; ++i) {
+			sum3 = 0;
 			for (int j = i + 1; j < n; ++j) {
-				
+				for (int k = 0; k < m; ++k) {
+					sum3 += v[j][k];
+				}
 			}
-
+			for (int k = 0; k < m - 1; ++k) {
+				sum1 = 0, sum2 = 0;
+				for (int l = 0; l < k + 1; ++l) {
+					for (int j = 0; j < i + 1; ++j) {
+						sum1 += v[j][l];
+					}
+				}
+				for (int l = k + 1; l < m; ++l) {
+					for (int j = 0; j < i + 1; ++j) {
+						sum2 += v[j][l];
+					}
+				}
+				tmp = (ll)sum1 * (ll)sum2 * (ll)sum3;
+				ans = ans > tmp ? ans : tmp;
+			}
+		}
+	}
+	//case3,4
+	if (m > 1) {
+		for (int i = 0; i < m - 1; ++i) {
+			sum1 = 0;
+			for (int j = 0; j < i + 1; ++j) {
+				for (int k = 0; k < n; ++k) {
+					sum1 += v[k][j];
+				}
+			}
+			for (int k = 0; k < n - 1; ++k) {
+				sum2 = 0, sum3 = 0;
+				for (int l = 0; l < k + 1; ++l) {
+					for (int j = i + 1; j < m; ++j) {
+						sum2 += v[l][j];
+					}
+				}
+				for (int l = k + 1; l < n; ++l) {
+					for (int j = i + 1; j < m; ++j) {
+						sum3 += v[l][j];
+					}
+				}
+				tmp = (ll)sum1 * (ll)sum2 * (ll)sum3;
+				ans = ans > tmp ? ans : tmp;
+			}
+		}
+	}
+	if (m > 1) {
+		for (int i = 0; i < m - 1; ++i) {
+			sum3 = 0;
+			for (int j = i + 1; j < m; ++j) {
+				for (int k = 0; k < n; ++k) {
+					sum3 += v[k][j];
+				}
+			}
+			for (int k = 0; k < n - 1; ++k) {
+				sum1 = 0, sum2 = 0;
+				for (int l = 0; l < k + 1; ++l) {
+					for (int j = 0; j < i + 1; ++j) {
+						sum1 += v[l][j];
+					}
+				}
+				for (int l = k + 1; l < n; ++l) {
+					for (int j = 0; j < i + 1; ++j) {
+						sum2 += v[l][j];
+					}
+				}
+				tmp = (ll)sum1 * (ll)sum2 * (ll)sum3;
+				ans = ans > tmp ? ans : tmp;
+			}
 		}
 	}
 
-	
+
 	cout << ans << '\n';
 
 	return 0;
